4745668fb86213b4ae22ed8b00b6569d0351d3f9
1Dimitri <git.dimitri@janczak.net>
2019-12-20 13:31:17 +0100

    Create pg.cmd

---
+++ WindowsConsole/pg.cmd
@@ -0,0 +1,13 @@
+
+@echo off
+REM Standard Environment variables to work with PostgreSQL
+REM 
+REM Tested v11.6
+REM
+REM DJ - 11-Dec-2019 - 1.0 - Initial release
+
+REM Codepage
+chcp 1252
+
+REM Force english prompts and messages
+set LC_MESSAGES=English
